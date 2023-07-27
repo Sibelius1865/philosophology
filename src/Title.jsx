89292403e5ac90fab23858a2077cd4c7a40183cd
たいようはらだ <49637219+Sibelius1865@users.noreply.github.com>
2023-07-27 16:34:16 +0900

    Delete src directory

--- src/Title.jsx
+++
@@ -1,9 +0,0 @@
-const Title = ({scrollPosition}) => {
-  return (
-    <div className='area'>
-      <div className='title'>INTRODUCTION TO PHILOSOPHOLOGY</div>
-    </div>
-  )
-}
-
-export default Title
